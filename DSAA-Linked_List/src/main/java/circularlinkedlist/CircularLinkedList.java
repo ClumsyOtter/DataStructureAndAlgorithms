6459311228ee1525138ca623d45755426cf556e1
ClomsyOtter <chengrui2017@126.com>
2017-03-14 14:33:08 +0800

    循环链表

---
+++ DSAA-Linked_List/src/main/java/circularlinkedlist/CircularLinkedList.java
@@ -0,0 +1,171 @@
+package circularlinkedlist;
+
+//循环链表
+public class CircularLinkedList<E> {
+
+	private Node<E> headNode;
+	private Node<E> endNode;
+
+	// 在链表表尾增加链表节点
+	public void add(E obj) {
+		Node<E> node = new Node<>();
+		node.setData(obj);
+		// 如果链表为空 则首尾节点都指向第一个节点
+		if (headNode == null) {
+			headNode = node;
+			endNode = node;
+			//循环链表的尾节点指向头结点
+			endNode.setNext(headNode);
+		} else {
+			// 不为空 将尾节点指向新增节点
+			endNode.setNext(node);
+			//新增节点指向头节点
+			node.setNext(headNode);
+			// 尾节点后移
+			endNode = node;
+		}
+	}
+
+	// 在指定中间位置增加节点
+	public void add(int index, E element) {
+		Node<E> node = new Node<>();
+		node.setData(element);
+
+		if (headNode == null) {
+			throw new NullPointerException("空链表");
+		}
+		// 判断位置是否超出界限
+		else if ((index < 0) || (index > this.size())) {
+			throw new ArrayIndexOutOfBoundsException("超出范围");
+		} else {
+			// 首节点插入
+			if (index == 0) {
+				node.setNext(headNode);
+				endNode.setNext(node);
+				headNode = node;
+			}
+			// 尾节点插入
+			else if (index == this.size()) {
+				endNode.setNext(node);
+				node.setNext(headNode);
+				endNode = node;
+			}
+			// 中间插入
+			else {
+				Node<E> hnode = headNode;
+				// 将首节点移动到插入节点的前一个节点
+				for (int i = 1; i <= index - 1; i++) {
+					hnode = hnode.getNext();
+				}
+				node.setNext(hnode.getNext());
+				hnode.setNext(node);
+			}
+		}
+	}
+
+	// 移除此列表中指定位置上的元素
+	public void remove(int index) {
+
+		if (headNode == null) {
+			throw new NullPointerException("空链表");
+		} else if ((index < 0) || (index >= this.size())) {
+			throw new ArrayIndexOutOfBoundsException("超出范围");
+		} else {
+			// 保护头结点
+			Node<E> hnode = headNode;
+			// 删除首节点
+			if (index == 0) {
+				//更新首节点 
+				headNode = headNode.getNext();
+				//更新尾节点
+				endNode.setNext(headNode);
+				// System.out.println(index + "号：" + hnode.getData().toString()
+				// + "移除成功");
+			}
+			// 删除尾节点
+			else if (index == (this.size() - 1)) {
+				while (hnode.getNext() != endNode) {
+					hnode = hnode.getNext();
+				}
+				hnode.setNext(headNode);
+				// System.out.println(index + "号：" + endNode.getData() + "
+				// 移除成功");
+				endNode = hnode;
+			}
+			// 删除中间节点
+			else {
+				for (int i = 1; i < index; i++) {
+					hnode = hnode.getNext();
+				}
+				// E data = hnode.getNext().getData();
+				hnode.setNext(hnode.getNext().getNext());
+				// System.out.println(index + "号：" + data.toString() + " 移除成功");
+			}
+		}
+	}
+
+	// 返回此列表中指定位置上的元素。
+	public E get(int index) {
+		if (headNode == null) {
+			throw new NullPointerException("空链表");
+		} else {
+			// 遍历不到尾节点
+			for (Node<E> node = headNode; node.getNext() != headNode; node = node.getNext()) {
+				if (index == 0) {
+					return node.getData();
+				}
+				index--;
+			}
+			// 尾节点单独检测 为什么是0因为index多运行了一次
+			if (index == 0) {
+				return endNode.getData();
+			} else
+				throw new ArrayIndexOutOfBoundsException("超出范围");
+		}
+
+	}
+
+	// 返回此列表的元素数。
+	public int size() {
+		//判断是否为空
+		if (headNode == null) {
+			return 0;
+		} else {
+			Node<E> node = headNode;
+			int count = 0;
+			//循环链表没有节点next为空
+			while (node.getNext() != headNode) {
+				node = node.getNext();
+				count++;
+			}
+			return count + 1;
+		}
+	}
+
+	// 用指定的元素替代此列表中指定位置上的元素。
+	public E set(int index, E element) {
+		// 删除指定位置的元素
+		this.remove(index);
+		// 插入指定位置元素
+		this.add(index, element);
+		return element;
+	}
+
+
+	public Node<E> getHeadNode() {
+		return headNode;
+	}
+
+	public void setHeadNode(Node<E> headNode) {
+		this.headNode = headNode;
+	}
+
+	public Node<E> getEndNode() {
+		return endNode;
+	}
+
+	public void setEndNode(Node<E> endNode) {
+		this.endNode = endNode;
+	}
+
+}
