5c3318fac707361818a03e645197424f72b1ab73
ClomsyOtter <chengrui2017@126.com>
2017-03-28 16:41:47 +0800

    测试

---
+++ DSAA-Hash/src/test/java/hash/test/TestHash.java
@@ -0,0 +1,18 @@
+package hash.test;
+
+import org.junit.Test;
+
+import com.hash.separatechaining.DoubleQuadraticProbingHashTable;
+
+public class TestHash {
+	@Test
+	public void test() {
+		DoubleQuadraticProbingHashTable<Integer> dHashTable = new DoubleQuadraticProbingHashTable<>();
+		for (int i = 0; i < 15; i++)
+			dHashTable.insert(i);
+		dHashTable.remove(5);
+		for (int i = 0; i < 15; i++)
+			System.out.println(i + " 是否存在  " + dHashTable.contain(i));
+	}
+
+}
